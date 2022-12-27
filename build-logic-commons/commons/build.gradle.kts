a7b53c45a9be384692901d889261e122e5cff3ef
Bo Zhang <bo@gradle.com>
2022-12-27 10:47:18 +0800

    Troubleshoot cache miss

--- build-logic-commons/commons/build.gradle.kts
+++ build-logic-commons/commons/build.gradle.kts
@@ -12,3 +12,14 @@ java {
         vendor.set(JvmVendorSpec.ADOPTIUM)
     }
 }
+
+tasks.named<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlin") {
+    doFirst {
+        println("""classpathSnapshotProperties:
+            |${classpathSnapshotProperties.useClasspathSnapshot.orNull}
+            |${classpathSnapshotProperties.classpathSnapshot.files}
+            |${classpathSnapshotProperties.classpath.files}
+            |${classpathSnapshotProperties.classpathSnapshotDir.orNull}""".trimMargin())
+        println("toolchain: ${kotlinJavaToolchainProvider.get().javaVersion.orNull}")
+    }
+}
